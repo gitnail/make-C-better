5a9e4ed136e6404c1e647cc48af4862737996a37
gitnail <n.nail15@mail.ru>
2018-05-10 00:01:53 +0300

    my set and map

--- my_set_and_map.cpp
+++ my_set_and_map.cpp
@@ -1,5 +1,5 @@
 template <typename TKey>
-class Set : public set<TKey> {
+class TSet : public set<TKey> {
 public:
     inline bool has(const TKey& key) const {
         return this->find(key) != this->end();
@@ -7,7 +7,7 @@ public:
 };
 
 template <typename TKey, typename TValue>
-class Map : public map<TKey, TValue> {
+class TMap : public map<TKey, TValue> {
 public:
     inline bool has(const TKey& key) const {
         return this->find(key) != this->end();
